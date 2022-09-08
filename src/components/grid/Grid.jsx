0a655e29f05b55d91370328bd748d80dbde02ffe
Ashish Singh <ashusingh15082001@gmail.com>
2022-09-08 15:40:29 +0530

    1.5.3
    added to properties text in rent page.

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -6,13 +6,18 @@ function Grid({ propertiesIds }) {
   // console.log(propertiesIds);
   return (
     <div className={styles["grid-container"]}>
-      <ul className={styles["grid"]}>
-        {propertiesIds.map((propertyId) => (
-          <li className={styles["grid-item"]} key={propertyId}>
-            <Card propertyId={propertyId} />
-          </li>
-        ))}
-      </ul>
+      {propertiesIds.length === 0 && (
+        <p className={styles["grid-container-message"]}>No Properties found</p>
+      )}
+      {propertiesIds.length > 0 && (
+        <ul className={styles["grid"]}>
+          {propertiesIds.map((propertyId) => (
+            <li className={styles["grid-item"]} key={propertyId}>
+              <Card propertyId={propertyId} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
